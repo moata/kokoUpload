8690a41ca9eb933c8c94dd15e19400a06e501cbd
Moatasem <mabdalmahdi@gmail.com>
2018-08-03 21:43:25 +0300

    run checksum function before flask start

--- main.py
+++ main.py
@@ -11,41 +11,31 @@ import pickle
 import hashlib
 hasher = hashlib.sha1()
 
-api = Flask(__name__)
-api.config.from_envvar('APPLICATION_SETTINGS')
-
-
-
-if not os.path.exists(api.config['UPLOAD_DIR']):
-    os.makedirs(api.config['UPLOAD_DIR'])
-
+BLOCKSIZE=65536
+UPLOAD_DIR = '/app/uploadedFiles/'
 
 # calculate checksum for a file 
 def genCheckSumForFiles(names,root):
-    
     with open(os.path.join(root,names),'rb') as afile:
-        buf = afile.read(api.config['BLOCKSIZE'])
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = afile.read(api.config['BLOCKSIZE'])
+        try:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        except:
+            print 'ERROR: Reading file !'
     return hasher.hexdigest()
 
-# calculate a checksum for data string
-def genCheckSum(uploadedFile):
-    return hashlib.sha1(uploadedFile.read()).hexdigest()
-            
-    
 def checkSumCalc(dbFile):
     try:
-        for root,dirs,files in os.walk(api.config['UPLOAD_DIR']):
+        for root,dirs,files in os.walk(UPLOAD_DIR):
             for names in files:
-                fileUUID=names.rsplit('.', 1)[0]
-                if fileUUID in dbFile:
-                    pass
+                if names in dbFile:
+                    print 'already there !'
                 else:
                     md5 = genCheckSumForFiles(names,root)
                     
-                    dbFile.update({md5:fileUUID})
+                    dbFile.update({md5:names})
                     output = open("db.pkl","wb")
                     pickle.dump(dbFile,output)
                     output.close()
@@ -53,7 +43,6 @@ def checkSumCalc(dbFile):
         pkl_file = open('db.pkl', 'rb')
         mydict = pickle.load(pkl_file)
         pkl_file.close()
-        print mydict
     except:
         pass
     
@@ -62,21 +51,29 @@ def checkSumDbFile():
     try:
         dbFile = pickle.load(open("db.pkl","rb"))
         checkSumCalc(dbFile)
+        return Flask(__name__) 
     except (OSError, IOError) as e:
         foo = {'a':0}
         pickle.dump(foo,open("db.pkl","wb"))
         dbFile = pickle.load(open("db.pkl","rb"))
         checkSumCalc(dbFile)
+        return Flask(__name__)
 
-# Create a custom server to calculate checksum before start receiving request.
-class CustomServer(Server):
-    def __call__(self,app,*args,**kwargs):
-        checkSumDbFile()
-        return Server.__call__(self,app,*args,**kwargs)
+api = checkSumDbFile()
+api.config.from_envvar('APPLICATION_SETTINGS')
 
-manager = Manager(api)
-manager.add_command('runserver',CustomServer(host=api.config['HOST'],port=api.config['PORT']))
 
+
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
+
+
+
+# calculate a checksum for data string
+def genCheckSum(uploadedFile):
+    return hashlib.sha1(uploadedFile.read()).hexdigest()
+            
 
 
 # check if file extention is allowed 
@@ -86,13 +83,31 @@ def allowed_file(filename):
 
 
 
+# Delete file
+@api.route('/files/del/<filename>')
+def delete_file(filename):
+    if os.path.exists(os.path.join(UPLOAD_DIR,filename)):
+        try:
+            os.remove(os.path.join(UPLOAD_DIR,filename))
+            dbFile = pickle.load(open("db.pkl","wb"))
+            for key, value in dbFile.iteritems():
+                if filename == value:
+                    del dbFile[value]
+                    output2 = open("db.pkl","wb")
+                    pickle.dump(dbFile,output2)
+                    output2.close()
+            return 'Deleted\n',201
+        except:
+            return 'Not able to delete',403
+    else:
+        return 'File not exists ',404
 
 # List files on the server 
 @api.route('/files')
 def list_files():
     files = []
-    for filename in os.listdir(api.config['UPLOAD_DIR']):
-        path = os.path.join(api.config['UPLOAD_DIR'],filename)
+    for filename in os.listdir(UPLOAD_DIR):
+        path = os.path.join(UPLOAD_DIR,filename)
         if os.path.isfile(path):
             files.append(filename)
     return jsonify(files)
@@ -100,7 +115,7 @@ def list_files():
 # Download a file
 @api.route('/files/<path:path>')
 def get_file(path):
-    return send_from_directory(api.config['UPLOAD_DIR'],path,as_attachment=True)
+    return send_from_directory(UPLOAD_DIR,path,as_attachment=True)
 
 # Upload a file
 @api.route('/files/<filename>',methods=['POST'])
@@ -118,21 +133,21 @@ def post_file(filename):
         # open the database file to check if this hash already exists
         output = pickle.load(open("db.pkl","rb"))
         if requestMD5 in output:
-            return output[requestMD5]+'\n',201
+            return "http://"+request.host+"/files/"+output[requestMD5]+'\n',201
         else:
             # generate uuid for the new file 
             Nfilename = urlsafe_b64encode(uuid4().bytes).decode("ascii").rstrip("=")
-            with open(os.path.join(api.config['UPLOAD_DIR'],Nfilename+"."+extension),'wb') as fp:
+            with open(os.path.join(UPLOAD_DIR,Nfilename+"."+extension),'wb') as fp:
                 fp.write(request.data)
             # upload the checksum in database file 
             output.update({requestMD5:Nfilename+"."+extension})
             output2 = open("db.pkl","wb")
             pickle.dump(output,output2)
             output2.close()
-        return Nfilename+"."+extension+'\n',201
+        return "http://"+request.host+"/files/"+Nfilename+"."+extension+'\n',201
     else:
         return abort(400,'not allowed extension')
 
 
 if __name__ == '__main__':
-    manager.run()
+    api.run(host='0.0.0.0',port='5000')
